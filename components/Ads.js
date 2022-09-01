721bab0855df50c698d5f2606745c9cb6d527008
[Muhammad Husnain] <[husnaindev4@gmail.com]>
2022-09-02 01:10:10 +0500

    added src

--- components/Ads.js
+++ components/Ads.js
@@ -2,7 +2,7 @@ export default function Ads() {
   return (
     <script
       async
-      script-src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8108156981776188"
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8108156981776188"
       crossOrigin="anonymous"
     ></script>
   )
